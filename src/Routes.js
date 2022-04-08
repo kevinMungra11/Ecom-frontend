95ca80a4845da57d042e25c8884dca3e96aba4a5
kevinMungra11 <kp496719@gmail.com>
2022-04-08 19:10:11 +0530

    Frontend for t-shirt store

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,10 @@ import UserDashBoard from './user/UserDashBoard';
 import AddCategory from './admin/AddCategory';
 import ManageCategories from './admin/ManageCategories';
 import AddProduct from './admin/AddProduct';
+import ManageProducts from './admin/ManageProducts'
+import UpdateProduct from './admin/UpdateProduct';
+import Cart from './core/Cart';
+// import { updateProduct } from './admin/helper/adminapicall';
 
 const Routes = () => {
   return (
@@ -18,11 +22,14 @@ const Routes = () => {
             <Route path='/' exact component = {Home}/>
             <Route path='/signup' exact component = {Signup}/>
             <Route path='/signin' exact component = {Signin}/>
+            <Route path='/cart' exact component = {Cart}/>
             <PrivateRoutes path='/user/dashboard' exact component = {UserDashBoard}/>
             <AdminRoute path='/admin/dashboard' exact component = {AdminDashBoard}/>
             <AdminRoute path='/admin/create/category' exact component = {AddCategory}/>
             <AdminRoute path='/admin/categories' exact component = {ManageCategories}/>
             <AdminRoute path='/admin/create/product' exact component = {AddProduct}/>
+            <AdminRoute path='/admin/products' exact component = {ManageProducts}/>
+            <AdminRoute path='/admin/product/update/:productId' exact component = {UpdateProduct}/>
         </Switch>
     </BrowserRouter>
   )
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import Card from './Card';
+import { getProduct } from '../core/helper/coreapicalls';
+
 
 export default function Home() {
-  console.log("API IS", API);
+
+  const [products, setProducts] = useState([]);
+
+  const [error, setError] = useState(false);
+
+  const loadAllProduct = () => {
+    getProduct()
+    .then((data) => {
+      if(data.error){
+        setError(data.error);
+      }
+      else{
+        setProducts(data);
+      }
+    })
+  }
+
+  useEffect(() => {
+    loadAllProduct();
+  }, [])
+  
 
   return (
-    <Base title="Home Page" description="Welcome to the Tshirt Store">
-      <div className="row">
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+    <Base titel="Home Page" description="Welcome to the Tshirt Store">
+      <div className="row text-center">
+        <h1 className="text-white">All of t-shirts</h1>
+        <div className="row">
+          {products.map((product,index) => {
+            return (
+              <div key={index} className="col-4 mb-4">
+                <Card product={product} />
+              </div>
+            )
+          })}
         </div>
       </div>
     </Base>
